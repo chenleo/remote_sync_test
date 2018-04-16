5fd3caee53f97d53b5271d8cc66034bd37b8c6c3
Leo Chen <maverickcc@gmail.com>
2018-04-16 12:01:47 +0800

    add general info in test

---
+++ test/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# encoding: utf-8
+# __init__.py.py in remote_sync_test
+# Created by maverick on 2018/4/16, 11:59 AM
+# 
+
+"""
+@version: ??
+@author: Leo Chen
+@contact: maverickcc@gmail.com
+@file: __init__.py.py
+@time: 2018/4/16 11:59 AM
+"""
---
+++ test/general_info.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# encoding: utf-8
+# general_info.py in remote_sync_test
+# Created by maverick on 2018/4/16, 12:00 PM
+# 
+
+"""
+@version: ??
+@author: Leo Chen
+@contact: maverickcc@gmail.com
+@file: general_info.py
+@time: 2018/4/16 12:00 PM
+"""
+
+import sys
+print(sys.version_info)
